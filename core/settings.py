ee807c9d61610c3400f8f80cd8ad8bcee43d92dc
robert <zeonita@gmail.com>
2024-11-19 03:00:00 +0200

    Update SECRET_KEY handling and modify event title in home template

--- core/settings.py
+++ core/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a2xy@gzd-_1u^qapbh9q@qi#kk0+hd!$!w_#9*+!bvf$ke(k&o"
-#SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = "django-insecure-a2xy@gzd-_1u^qapbh9q@qi#kk0+hd!$!w_#9*+!bvf$ke(k&o"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
