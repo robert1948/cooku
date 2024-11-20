8b5e237221e7b17681e0515ab964c4727d67fc67
robert <zeonita@gmail.com>
2024-11-20 05:36:24 +0200

    Refactor venue management: implement CSV, text, and PDF export features; update SECRET_KEY handling; enhance pagination in venue listing

--- events/urls.py
+++ events/urls.py
@@ -15,5 +15,7 @@ urlpatterns = [
     path('update_event/<event_id>',views.update_event,name='update-event'),
     path('delete_event/<event_id>',views.delete_event,name='delete-event'),
     path('delete_venue/<venue_id>',views.delete_venue,name='delete-venue'),
-
+    path('venue_text',views.venue_text,name='venue-text'),
+    path('venue_csv',views.venue_csv,name='venue-csv'),
+    path('venue_pdf',views.venue_pdf,name='venue-pdf'),
 ]
--- events/views.py
+++ events/views.py
@@ -5,8 +5,86 @@ from calendar import HTMLCalendar
 from .forms import VenueForm, EventForm
 from .models import Event, Venue
 from django.http import HttpResponseRedirect
-
+from django.http import HttpResponse
+import csv
 from django.views.decorators.csrf import csrf_protect
+
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import inch
+
+# Import Pagination Stuff
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+# Generate a PDF file Venue List
+def venue_pdf(request):
+    #Create a Bytestream buffer
+    buf = io.BytesIO()
+    # Create a canvas object
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    # Create a text object
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica", 10)
+
+    # Designate The Model
+    venues = Venue.objects.all()
+    # Add some lines of text
+    lines = []
+    # Loop through the data and write it to the PDF file
+    for venue in venues:
+        lines.append(venue.name)
+        lines.append(venue.address)
+        lines.append(venue.zip_code)
+        lines.append(venue.phone)
+        lines.append(venue.web)
+        lines.append(venue.email_address)
+        lines.append(" ")
+
+    # Loop
+    for line in lines:
+        textob.textLine(line)
+        
+    # Finish up
+    textob.textLines(lines)
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+
+    return FileResponse(buf, as_attachment=True, filename='venue.pdf')
+
+
+
+# Generate Text File and CSV File
+def venue_csv(request):
+    venue_list = Venue.objects.all()
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="venue.csv"'
+    # Create a CSV writer using the response object
+    writer = csv.writer(response)
+    # Des the model fields as the header row
+    venues = Venue.objects.all()
+    # Add column headings to the CSV file
+    writer.writerow(['Name', 'Web', 'Address'])
+    lines = []
+    # Loop through the data and write it to the CSV file
+    for venue in venue_list:
+        writer.writerow([venue.name, venue.web, venue.address])
+    return response
+
+
+def venue_text(request):
+    venue_list = Venue.objects.all()
+    response = HttpResponse(content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename="venue.txt"'
+    for venue in venue_list:
+        response.write(venue.name + '\n')
+        response.write(venue.web + '\n')
+        response.write(venue.address + '\n\n')
+    return response
 
 def delete_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
@@ -39,6 +117,8 @@ def add_event(request):
         if 'submitted' in request.GET:
             submitted = True
     return render(request, 'events/add_event.html', {'form': form, 'submitted': submitted})
+
+
 
 def update_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
@@ -74,8 +154,14 @@ def show_venue(request, venue_id):
     return render(request, 'events/show_venue.html', {'venue': venue})
 
 def list_venues(request):
+    #venue_list = Venue.objects.all().order_by('name')
     venue_list = Venue.objects.all()
-    return render(request, 'events/venue.html', {'venue_list': venue_list})
+    # Pagination
+    p = Paginator(Venue.objects.all(), 2)
+    page = request.GET.get('page')
+    venues = p.get_page(page)
+    return render(request, 'events/venue.html', {'venue_list': venue_list, 'venues': venues})
+    #return render(request, 'events/venue.html', {'venue_list': venue_list})
 
 def add_venue(request):
     submitted = False
